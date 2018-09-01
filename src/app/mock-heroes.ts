a5277c25896f2566089986717b4415d9d5e40cef
Ram Prasad <tmg.designer@tmgindia.in>
2018-09-01 10:47:27 +0530

    tour-of-heroes first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 1, name: 'Mr. Nice'},
+	{ id: 2, name: 'Mr. Perfect'},
+	{ id: 3, name: 'Mrs. Perfect'},
+	{ id: 4, name: 'Mr. Ram'},
+	{ id: 5, name: 'Mrs. Ram'},
+	{ id: 6, name: 'Mr. HNB'},
+	{ id: 7, name: 'Mrs. HNB'},
+	{ id: 8, name: 'Mr. Bright'},
+	{ id: 9, name: 'Mr. Cool'},
+	{ id: 10, name: 'Mr. Hot'},
+];
+
